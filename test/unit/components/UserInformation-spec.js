c6a8fb0f0536bbc3dd9d65545f11d55e6cecdfe9
Rory Devane <rory.devane@gmail.com>
2017-05-09 18:30:06 +0100

    refactored UserInformation-spec

--- test/unit/components/UserInformation-spec.js
+++ test/unit/components/UserInformation-spec.js
@@ -3,84 +3,89 @@
 import React from "react";
 import {shallow} from "enzyme";
 import {UserInformation} from "../../../src/components/UserInformation";
-import configureStore from 'redux-mock-store';
 import fetchMock from 'fetch-mock'
-
 import * as userActions from "../../../src/actions/userActions";
-
-const mockStore = configureStore([])
-
-function setup() {
-
-    const props = {
-        userlist: {
-            users: []
-        }
-    };
-
-    const enzymeWrapper = shallow(<UserInformation {...props}/>);
-
-    return {
-        props,
-        enzymeWrapper
-    };
-}
 
 describe("UserInformation Component", () => {
 
-    it("should render self", () => {
-        const {enzymeWrapper, props} = setup();
-        expect(enzymeWrapper.find(".user-information").hasClass("user-information")).toBe(true);
-    });
+    describe('render', () => {
 
-    it("should call dispatch with correct user actions", () => {
+        function setup() {
+
+            const props = {
+                userlist: {
+                    users: []
+                }
+            };
+
+            const enzymeWrapper = shallow(<UserInformation {...props}/>);
+
+            return {
+                props,
+                enzymeWrapper
+            };
+        }
+
+        it("should render self", () => {
+            const {enzymeWrapper, props} = setup();
+            expect(enzymeWrapper.find(".user-information").hasClass("user-information")).toBe(true);
+        });
+    })
+
+    describe('fetchMock', () => {
 
         const mockDispatch = jest.fn();
         const component = new UserInformation({dispatch: mockDispatch, userlist: {}});
 
-        fetchMock.get('http://localhost:3001/users', [{
-                "id": 1,
-                "givenName": "Peter",
-                "familyName": "Capaldi"
-            },
-            {
-                "id": 2,
-                "givenName": "Matt",
-                "familyName": "Smith"
-            }]);
+        afterEach(() => {
+            fetchMock.restore();
+        });
 
+        it("should call dispatch with correct user actions", () => {
 
-        return component.fetchData().then(data => {
-                expect(mockDispatch).toBeCalledWith(userActions.addUserData({
+            fetchMock.get('http://localhost:3001/users', [{
                     "id": 1,
                     "givenName": "Peter",
                     "familyName": "Capaldi"
-                },));
-                expect(mockDispatch).toBeCalledWith(userActions.addUserData({
+                },
+                {
                     "id": 2,
                     "givenName": "Matt",
                     "familyName": "Smith"
-                },));
-                fetchMock.restore();
-            }
-        );
+                }]);
 
-    });
 
-    it("should try 3 times to make another request if the first one fails", () => {
+            return component.fetchData().then(data => {
+                    expect(mockDispatch).toBeCalledWith(userActions.addUserData({
+                        "id": 1,
+                        "givenName": "Peter",
+                        "familyName": "Capaldi"
+                    },));
+                    expect(mockDispatch).toBeCalledWith(userActions.addUserData({
+                        "id": 2,
+                        "givenName": "Matt",
+                        "familyName": "Smith"
+                    },));
 
-        const mockDispatch = jest.fn();
-        const component = new UserInformation({dispatch: mockDispatch, userlist: {}});
+                }
+            );
 
-        fetchMock.get('http://localhost:3001/users', 500);
+        });
 
-        return component.fetchData().then(data => {
-            expect(fetchMock.calls().matched.length).toBe(3);
-            fetchMock.restore();
-            }
-        );
+        it("should try 3 times to make another request if the first one fails", () => {
 
-    });
+            fetchMock.get('http://localhost:3001/users', 500);
+
+            return component.fetchData().then(data => {
+                    expect(fetchMock.calls().matched.length).toBe(3);
+                }
+            );
+
+        });
+
+    })
+
+
 
 });
 
